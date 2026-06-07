ebcdf7db5a126a68e8d079977e9de3a0187670ff
Tomasz Dubiel <dubieltomasz420@gmail.com>
2026-06-07 16:35:01 +0200

    Update 2196. Create Binary Tree From Descriptions.cpp

--- 2196. Create Binary Tree From Descriptions.cpp
+++ 2196. Create Binary Tree From Descriptions.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     TreeNode* createBinaryTree(const std::vector<std::vector<int>>& descriptions) {
-        std::unordered_map<int , TreeNode*> nodeMap;
-        std::set<int> children;
+        std::array<TreeNode*, 100001> um{};
+        int nodeSum{};
 
-        for(const std::vector<int>& description : descriptions)
-        {
-            if(nodeMap.find(description[0]) == nodeMap.end())
-                nodeMap[description[0]] = new TreeNode(description[0]);
+        for(const auto& description : descriptions) {
+            if(um[description[0]] == nullptr) {
+                um[description[0]] = new TreeNode(description[0]);
+                nodeSum += description[0];
+            }
 
-            if(nodeMap.find(description[1]) == nodeMap.end())
-                nodeMap[description[1]] = new TreeNode(description[1]);
+            if(um[description[1]] == nullptr) {
+                um[description[1]] = new TreeNode(description[1]);
+                nodeSum += description[1];
+            }
 
-            description[2]? nodeMap[description[0]]->left = nodeMap[description[1]] : nodeMap[description[0]]->right = nodeMap[description[1]];
+            nodeSum -= description[1];
 
-            children.insert(description[1]);
+            if(description[2]) {
+                um[description[0]]->left = um[description[1]];
+            } else {
+                um[description[0]]->right = um[description[1]];
+            }
         }
 
-        for(const std::pair<int , TreeNode*>& element : nodeMap)
-            if(children.find(element.first) == children.end()) return element.second;
-
-        return nullptr;
+        return um[nodeSum];
     }
 };
